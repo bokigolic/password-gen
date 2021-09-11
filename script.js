bc351f508a473c1701494358938255bea14a1c5d
Bojan Golic <bokigolic32@gmail.com>
2021-09-11 02:36:59 -0400

    add functions to pass-gen

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+function generatePassword(len) {
+  var charsStr = "abcdifgjklmnopqwxzyABSDIFGJKLMNOPQXUYZ123456789!@#$%^&*"
+  var charsList = []
+  for (i = 0; i < charsStr.length-1; i++){
+    charsList.push(charsStr[i])
+  }
+
+  var randomPassStr = ""
+  for (var i = 0; i < len-1; i++){
+    randomPassStr += charsList[Math.floor(Math.random()*charsList.length)]
+  }
+  
+  return randomPassStr;
+  }
+  
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordNum = window.prompt("How many caracter you want?");
+
+  var password = generatePassword(passwordNum.length);
+  // var passwordText = document.querySelector("#password");
+  var passwordText = document.getElementById("password");
 
   passwordText.value = password;
 
@@ -12,3 +30,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var textField = document.getElementById("password")
